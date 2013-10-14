b405630c63a8ec1d15f6699d90b6e9c79e37ad8c
Kristleifur <batwigg@gmail.com>
2013-10-14 15:19:50 +0000

    fileunifier done

--- fileUnifier.py
+++ fileUnifier.py
@@ -1,2 +1,11 @@
-def unifyFile(chunks):
-    return 0
+def unifyFile(chunks, outfile):
+    f = open(outfile, 'wb')
+    
+    for chunk in chunks:
+        f.write(chunk)
+    
+
+#import fileSplitter
+
+#chunks = fileSplitter.splitFile("doge.jpg", 512)
+#unifyFile(chunks, "anotherdoge.jpg")
