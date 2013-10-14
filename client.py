5e3bf291507c6bb953c2d99cd7868a389d17bd10
Kristleifur <batwigg@gmail.com>
2013-10-14 17:48:50 +0000

    pushy push

--- client.py
+++ client.py
@@ -1,32 +1,41 @@
 import socket
+import random
 
-def readFile(serverURL, filename, port):
+def read(serverURL, filename, port):
     sock = openConnection(serverURL, port)
     
-    sock.sendTo(RRQ(filename), host)
-
-def writeFile(serverURL, filename, port):
+    host = (serverURL, port)
+    string = RRQ(filename)
+    sock.sendto(string, host)
+    
+    data, addr = sock.recvfrom(5000)
+    print (addr)
+    print(data)
+    
+def write(serverURL, filename, port):
     pass
 
 def openConnection(serverURL, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    randport = random.randrange(40000, 59999)
+    #sock.bind(('127.0.0.1', randport))
     return sock
 
 def RRQ(filename):
     s = ""
     s += '01 '
-    s += 'octet '
-    s += '0 '
-    s += mode
+    s += filename
+    s += ' 0 '
+    s += 'octet'
     s += ' 0'
     return s
 
 def WRQ(filename):
     s = ""
     s += '02 '
-    s += 'octet '
-    s += '0 '
-    s += mode
+    s += filename
+    s += ' 0 '
+    s += 'octet'
     s += ' 0'
     return s
 
